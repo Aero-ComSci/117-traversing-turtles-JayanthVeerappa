b140c2a20ffc3348f9260cbd511c3e3ff7121005
JayanthVeerappa <132962391+JayanthVeerappa@users.noreply.github.com>
2024-09-27 13:27:14 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,66 +1,34 @@
-#   a117_traversing_turtles.py
-#   Add code to make turtles move in a circle and change colors.
 import turtle as trtl
-# create an empty list of turtles
-my_turtles = []
-# use interesting shapes and colors
 
-class tortle():
-    def __init__(self, shapes, colors, direction, startx , starty ):
-        self.shape = shapes
-        self.colors = colors
-        self.direction = direction
-        self.startx = startx
-        self.starty = starty
-        
-    def attribute(self):
-        for s in turtle_shapes:
-            t = trtl.Turtle(shape=s)
-            my_turtles.append(t)
-            new_color = self.color.pop()
-            t.color(new_color)
-            t.penup()
+class Tortle():
+  def __init__(self,shapes,colors,my_turtles,startx,starty, direction):
+    self.shapes = shapes
+    self.colors = colors
+    self.turtles = my_turtles
+    self.sx = startx
+    self.sy = starty
+    self.direction = direction
 
+  def setShapeColor(self):
+    for s in self.shapes:
+      t = trtl.Turtle(shape=s)
+      new_color = self.colors.pop()
+      t.color(new_color)
+      t.penup()
+      self.turtles.append(t)
 
+  def move_turtle(self):
+    for t in self.turtles:
+      t.goto(self.sx, self.sy)
+      t.pendown()
+      t.setheading(self.direction)
+      t.right(45)    
+      t.forward(50)
 
+      self.direction = t.heading()
 
+      self.sx = t.xcor()
+      self.sy = t.ycor()
 
-
-
-
-for s in turtle_shapes:
-    t = trtl.Turtle(shape=s)
-    my_turtles.append(t)
-    new_color = turtle_colors.pop()
-    t.color(new_color)
-    t.penup()
-
-
-
-
-
-
-
-#setting intital position
-startx = 0
-starty = 0
-
-#
-
-direction = 90
-for t in my_turtles:
-    t.goto(startx, starty)
-    t.pendown()
-    t.setheading(direction)
-    t.right(45)     
-    t.forward(50)
-    direction = t.heading()
-
-
-
-#	
-startx = t.xcor()
-starty = t.ycor()
- 
-wn = trtl.Screen()
-wn.mainloop()
+  def __str__(self):
+    return f"Turtle Shapes: {self.shapes}"
